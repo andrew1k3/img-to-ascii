5475f75961487c226a6855051a3326fdae5f22a7
andrew1k3 <andrew.jgardner17@gmail.com>
2024-03-07 23:08:51 +1100

    buggy back end + new front

--- app.js
+++ app.js
@@ -1,25 +1,150 @@
-const input = document.getElementById("inputTag");
-const imageName = document.getElementById("imageName");
+console.log("Andrew Gardner 2024");
+
+const imageInput = document.getElementById("imageInput");
+const imageText = document.getElementById("imageText");
 const buttonEle = document.getElementById("convertButton");
 const test = document.getElementById("test");
 const canvas = document.getElementById("myCanvas");
+const slider = document.getElementById("slider");
+const sliderText = document.getElementById("sliderText");
+const result = document.getElementById("result");
 const ctx = canvas.getContext("2d");
 let image = null;
+let resolution = 0.5;
+let asciiImage = null;
 
-console.log("Script loaded");
+class AsciiPixel {
+  letterBrightnessArray = [
+    "`",
+    ".",
+    ",",
+    "-",
+    "+",
+    "=",
+    "|",
+    "?",
+    "$",
+    "%",
+    "@",
+    "#",
+  ];
+  constructor(r, g, b, a) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+    this.brightness = this.getBrightness();
+    this.letter = this.getLetter();
+  }
 
-input.addEventListener("change", () => {
-  console.log(1);
+  getBrightness() {
+    return (
+      (0.2126 * this.r + 0.7152 * this.g + 0.0722 * this.b) * (this.a / 256)
+    );
+  }
+
+  getLetter() {
+    return this.letterBrightnessArray[
+      Math.floor(this.brightness / (256 / this.letterBrightnessArray.length))
+    ];
+  }
+}
+
+class AsciiImage {
+  constructor(data, width, height) {
+    this.data = data;
+    this.width = width;
+    this.height = height;
+  }
+
+  generateAsciiPixelArray() {
+    let asciiPixelArray = [];
+    for (let i = 0; i < this.data.length; i += 4) {
+      asciiPixelArray.push(
+        new AsciiPixel(
+          this.data[i],
+          this.data[i + 1],
+          this.data[i + 2],
+          this.data[i + 3]
+        )
+      );
+    }
+    return asciiPixelArray;
+  }
+
+  averagePixel(array) {
+    let r,
+      g,
+      b,
+      a = 0;
+    array.forEach((pixel) => {
+      r += pixel.r / array.length;
+      g += pixel.g / array.length;
+      b += pixel.b / array.length;
+      a += pixel.a / array.length;
+    });
+    return new AsciiPixel(r, g, b, a);
+  }
+
+  // dont care abt res rn
+  generateScaledArray() {
+    console.log(this.data);
+    let cols = this.width;
+    let rows = this.height;
+    let asciiPixelArray = this.generateAsciiPixelArray();
+    let emptyArray = [...Array(rows)].map(() => Array(cols).fill(null));
+    for (let i = 0; i < asciiPixelArray.length - 1; i++) {
+      let pixel = asciiPixelArray[i];
+      // console.log(i, pixel, emptyArray[Math.floor(i / rows)][i % cols]);
+      try {
+        emptyArray[Math.floor(i / rows)][i % cols] = pixel;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    return emptyArray;
+  }
+
+  drawImage() {
+    let scaledArray = this.generateScaledArray();
+    result.innerHTML = "";
+    // console.log(scaledArray);
+    let fontSize = Math.floor((1 / resolution) * 10);
+    scaledArray.forEach((row) => {
+      let text = "";
+      row.forEach((pixel) => {
+        if (pixel) {
+          text += `<h4 style='color: rgb(${pixel.r}, ${pixel.g}, ${
+            pixel.b
+          });'>${pixel.getLetter()}</h4>`;
+        }
+      });
+      result.innerHTML += `<span>${text}</span>`;
+    });
+  }
+}
+
+// input image
+imageInput.addEventListener("change", () => {
   image = document.querySelector("input[type=file]").files[0];
-  imageName.innerText = image.name;
+  imageText.innerText = image.name;
   buttonEle.disabled = false;
-  console.log(image);
 });
 
+// slider resolution
+slider.addEventListener("input", (event) => {
+  sliderText.innerHTML = event.target.value + "%";
+  resolution = event.target.value / 100;
+  if (asciiImage) {
+    asciiImage.drawImage;
+  }
+});
+
+// button constructs image into cavas and reads data off canvas
 buttonEle.addEventListener("click", () => {
   if (image) {
     var reader = new FileReader();
-    reader.onload = function (event) {
+    reader.onload = function () {
       const img = new Image();
       test.src = reader.result;
       img.src = reader.result;
@@ -30,55 +155,15 @@ buttonEle.addEventListener("click", () => {
         ctx.drawImage(img, 0, 0);
 
         let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        drawData(imageData.data);
+        asciiImage = new AsciiImage(
+          imageData.data,
+          canvas.width,
+          canvas.height,
+          slider.value
+        );
+        asciiImage.drawImage();
       };
     };
     reader.readAsDataURL(image);
-
-    console.log(reader);
-
-    // ctx.drawImage(reader.result, 0, 0);
-    // var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    // var data = imageData.data;
-    // console.log(imageData);
   }
 });
-
-function drawData(data) {
-  let imageArray = [];
-  for (let i = 0; i < data.length; i += 4) {
-    let row = Math.floor(i / 4 / canvas.width);
-    let col = (i / 4) % canvas.width;
-    if (!imageArray[row]) {
-      imageArray[row] = [];
-    }
-    imageArray[row][col] = [data[i], data[i + 1], data[i + 2], data[i + 3]];
-  }
-  console.log(imageArray);
-
-  lowerResImageArray = [];
-  rows = Math.floor(imageArray.length / 10);
-  cols = Math.floor(imageArray[0].length / 10);
-
-  for (let i = 0; i < rows; i += 10) {
-    if (!lowerResImageArray[i / 10]) {
-      lowerResImageArray[i / 10] = [];
-    }
-
-    for (let j = 0; j < cols; j += 10) {
-      average = [0, 0, 0, 0];
-      for (let x = i; x < i + 10; x++) {
-        for (y = j; y < j + 10; y++) {
-          average = [
-            average[0] + imageArray[x][y][0] / 100,
-            average[1] + imageArray[x][y][1] / 100,
-            average[2] + imageArray[x][y][2] / 100,
-            average[3] + imageArray[x][y][3] / 100,
-          ];
-        }
-      }
-      lowerResImageArray[i / 10][j / 10] = average;
-    }
-  }
-  console.log(lowerResImageArray);
-}
